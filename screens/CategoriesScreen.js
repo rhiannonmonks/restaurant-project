2d2dd5555f988eb2e447896efc1283017c67b466
Rhiannon Monks <rhiannonlouisemonks@gmail.com>
2022-11-09 14:26:27 +0000

    creating a grid layout

---
+++ screens/CategoriesScreen.js
@@ -0,0 +1,26 @@
+import { FlatList } from "react-native";
+import CategoryGridTitle from "../components/CategoryGridTile";
+
+import { CATEGORIES } from "../data/dummy-data";
+
+function renderCategoryItem(itemData) {
+  return (
+    <CategoryGridTitle
+      title={itemData.item.title}
+      color={itemData.item.color}
+    />
+  );
+}
+
+function CategoriesScreen() {
+  return (
+    <FlatList
+      data={CATEGORIES}
+      keyExtractor={(item) => item.id}
+      renderItem={renderCategoryItem}
+      numColumns={2}
+    />
+  );
+}
+
+export default CategoriesScreen;
