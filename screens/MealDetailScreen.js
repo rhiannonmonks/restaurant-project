689a4fa42f845eb0bd3930cc5da3f94ca9254ce5
Rhiannon Monks <rhiannonlouisemonks@gmail.com>
2022-11-13 20:09:17 +0000

    setting navigation options dynamically

---
+++ screens/MealDetailScreen.js
@@ -0,0 +1,8 @@
+import { Text } from "react-native";
+
+function MealDetailScreen({ route }) {
+  const mealId = route.params.mealId;
+  return <Text>This is the Meal Detail Screen ({mealId})</Text>;
+}
+
+export default MealDetailScreen;
